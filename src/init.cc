a850c313440eedbb0bfc37da619809dd5ec860f5
Siarhei Ladzeika <s.ladeiko@aiby.com>
2023-12-06 16:30:06 +0300

    fix compilation under new nodejs versions

--- src/init.cc
+++ src/init.cc
@@ -9,15 +9,17 @@
 #include "memwatch.hh"
 
 extern "C" {
-    void init (v8::Handle<v8::Object> target)
+    void init (v8::Local<v8::Object> target)
     {
-        v8::HandleScope scope;
+        v8::Isolate * isolate = target->GetIsolate();
+
+        Nan::HandleScope scope;
         heapdiff::HeapDiff::Initialize(target);
 
-        NODE_SET_METHOD(target, "upon_gc", memwatch::upon_gc);
-        NODE_SET_METHOD(target, "gc", memwatch::trigger_gc);
+        Nan::SetMethod(target, "upon_gc", memwatch::upon_gc);
+        Nan::SetMethod(target, "gc", memwatch::trigger_gc);
 
-        v8::V8::AddGCEpilogueCallback(memwatch::after_gc);
+        isolate->AddGCEpilogueCallback(memwatch::after_gc);
     }
 
     NODE_MODULE(memwatch, init);
--- src/memwatch.hh
+++ src/memwatch.hh
@@ -6,12 +6,13 @@
 #define __MEMWATCH_HH
 
 #include <node.h>
+#include <nan.h>
 
 namespace memwatch
 {
-    v8::Handle<v8::Value> upon_gc(const v8::Arguments& args);
-    v8::Handle<v8::Value> trigger_gc(const v8::Arguments& args);
-    void after_gc(v8::GCType type, v8::GCCallbackFlags flags);
+    NAN_METHOD(upon_gc);
+    NAN_METHOD(trigger_gc);
+    void after_gc(v8::Isolate* isolate, v8::GCType type, v8::GCCallbackFlags flags);
 };
 
 #endif
